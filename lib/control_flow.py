db40e3fc8b92b59513586286c5261c8a0b36f2bf
Damien Nelson <dhnisaman@gmail.com>
2023-12-31 18:37:32 -0500

    created functions that passed test conditions

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,51 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if username.upper() == "ADMIN" and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature >= 40 and temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    # using lambda functions
+    # ops = {
+    #     "+": lambda x, y: x + y,
+    #     "-": lambda x, y: x - y,
+    #     "*": lambda x, y: x * y,
+    #     "/": lambda x, y: x / y,
+    # }
+
+    # if operation in ops:
+    #     return ops[operation](num1, num2)
+    # else:
+    #     return "Invalid operation"
+
+    # using eval
+    ops = ["+", "*", "-", "/"]
+
+    if operation in ops:
+        return eval(str(num1) + operation + str(num2))
+    else:
+       print("Invalid operation!")
+       return None
+
